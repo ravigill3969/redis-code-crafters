fa7079cfb2247113ca8d9116fbcfe0fea5229c2b
ravigill3936 <gill1234ravi@gmail.com>
2025-08-27 20:27:28 -0400

    added rpush

--- app/handlers/list.go
+++ app/handlers/list.go
@@ -1,9 +1,14 @@
 package handlers
 
+import "fmt"
+
 var RedisKeyArrayStore = map[string][]string{}
 
 func RPUSH(cmd []string) (int, error) {
+	fmt.Println(cmd)
 	RedisKeyArrayStore[cmd[0]] = append(RedisKeyArrayStore[cmd[0]], cmd[1])
 
-	return len(RedisKeyArrayStore[cmd[1]]), nil
+	fmt.Println(RedisKeyArrayStore[cmd[0]])
+
+	return len(RedisKeyArrayStore[cmd[0]]), nil
 }
