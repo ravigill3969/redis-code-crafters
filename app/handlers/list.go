fdeb80cf12020d1e12318525a68040fbc73cc5a8
ravigill3936 <gill1234ravi@gmail.com>
2025-08-27 20:49:32 -0400

    nice

--- app/handlers/list.go
+++ app/handlers/list.go
@@ -3,7 +3,9 @@ package handlers
 var RedisKeyArrayStore = map[string][]string{}
 
 func RPUSH(cmd []string) (int, error) {
-	RedisKeyArrayStore[cmd[0]] = append(RedisKeyArrayStore[cmd[0]], cmd[1])
+	key := cmd[0]
+	values := cmd[1:]
+	RedisKeyArrayStore[key] = append(RedisKeyArrayStore[key], values...)
 
-	return len(RedisKeyArrayStore[cmd[0]]), nil
+	return len(RedisKeyArrayStore[key]), nil
 }
