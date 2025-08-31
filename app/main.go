ac2a7139d7a4f0a12fc46ee11f8156c086cf32bf
ravigill3936 <gill1234ravi@gmail.com>
2025-08-31 15:14:47 -0400

    Xadd fixed indexing inside loop

--- app/main.go
+++ app/main.go
@@ -153,7 +153,9 @@ func handleConnection(conn net.Conn) {
 
 			res, ok := handlers.LPOP(cmdParser[1:])
 
-			if ok {
+			if len(res) == 1 {
+				fmt.Fprintf(conn, "$%d\r\n%s\r\n", len(res[0]), res[0])
+			} else if ok {
 				fmt.Fprintf(conn, "*%d\r\n", len(res))
 				for _, v := range res {
 					fmt.Fprintf(conn, "$%d\r\n%s\r\n", len(v), v)
