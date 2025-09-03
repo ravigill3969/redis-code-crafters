bbe30f05f24bc75f2bf47df539e0ede36c894b72
ravigill3936 <gill1234ravi@gmail.com>
2025-09-03 13:27:33 -0400

    info replication

--- app/main.go
+++ app/main.go
@@ -53,7 +53,8 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	buffer := make([]byte, 4096)
 
-	if strings.ToUpper(os.Args[3]) == "INFO" && strings.ToUpper(os.Args[4]) == "REPLICATION" {
+
+	if len(os.Args) == 5 && strings.ToUpper(os.Args[3]) == "INFO" && strings.ToUpper(os.Args[4]) == "REPLICATION" {
 		conn.Write([]byte("$11\r\nrole:master\r\n"))
 	}
 
