3c02856ef135d7f4d47ec2076871272314d95c89
ravigill3936 <gill1234ravi@gmail.com>
2025-09-04 14:45:07 -0400

    replication

--- app/main.go
+++ app/main.go
@@ -249,35 +249,37 @@ func readFromMaster(conn net.Conn) {
 			return
 		}
 
-		
 		raw := string(buffer[:n])
 		cmdParser := utils.ParseRESP(raw)
 		if len(cmdParser) == 0 {
 			continue
 		}
-		fmt.Println("recevied cmds insode read from amster", cmdParser )
+		fmt.Println("recevied cmds insode read from amster", cmdParser)
 
 		if handleReplicaCmd(cmdParser, conn) {
-            continue
-        }
+			continue
+		}
 
 		cmds.RunCmds(conn, cmdParser)
 	}
 }
 
 func handleReplicaCmd(cmdParser []interface{}, conn net.Conn) bool {
-    cmd := strings.ToUpper(fmt.Sprintf("%v", cmdParser[0]))
-    if cmd != "REPLCONF" {
-        return false
-    }
+	cmd := strings.ToUpper(fmt.Sprintf("%v", cmdParser[0]))
+	if cmd != "REPLCONF" {
+		return false
+	}
 
-    subcmd := strings.ToUpper(fmt.Sprintf("%v", cmdParser[1]))
-    switch subcmd {
-    case "LISTENING-PORT", "CAPA", "GETACK":
-        conn.Write([]byte("+OK\r\n"))
-        return true
-    default:
-        conn.Write([]byte("-ERR unknown REPLCONF subcommand\r\n"))
-        return true
-    }
+	subcmd := strings.ToUpper(fmt.Sprintf("%v", cmdParser[1]))
+	switch subcmd {
+	case "LISTENING-PORT", "CAPA":
+		conn.Write([]byte("+OK\r\n"))
+		return true
+
+	case "GETACK":
+		conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"))
+	default:
+		conn.Write([]byte("-ERR unknown REPLCONF subcommand\r\n"))
+		return true
+	}
 }
