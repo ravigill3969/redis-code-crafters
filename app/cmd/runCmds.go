a6ac35e49d0cdf60bf2ed3fe468ca9cd7d108d7e
ravigill3936 <gill1234ravi@gmail.com>
2025-09-04 13:06:30 -0400

    replication

--- app/cmd/runCmds.go
+++ app/cmd/runCmds.go
@@ -158,8 +158,7 @@ func RunCmds(conn net.Conn, cmdParser []interface{}) {
 		case "CAPA":
 			conn.Write([]byte("+OK\r\n"))
 		case "GETACK":
-			ackResp := "*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"
-			_, err := conn.Write([]byte(ackResp))
+			_, err := conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"))
 			if err != nil {
 				fmt.Println("Failed to send ACK:", err)
 			} else {
